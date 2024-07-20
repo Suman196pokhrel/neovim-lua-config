d4ea5cffeb261b0c259de580fdeb71f33aa2870c
Suman Pokhrel <spokhrel196@gmail.com>
2024-07-20 10:03:30 +0545

    updated vim config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,27 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
+-- vim-options.lua
+
+-- Global options
+vim.g.mapleader = " "  -- Set leader key
+
+-- Tab and indentation settings
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Interface settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+
+-- Search settings
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
