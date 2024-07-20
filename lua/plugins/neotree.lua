6a2502638dac4c38e4b4db8a1559604fbebe8412
Suman Pokhrel <spokhrel196@gmail.com>
2024-07-20 10:17:35 +0545

    added keymaps for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,7 +8,18 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+    vim.keymap.set("n", "<leader>rf", ":Neotree reveal<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 		vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>", { noremap = true, silent = true })
+
+		-- Neotree settings
+		require("neo-tree").setup({
+
+			close_if_last_window = true,
+      popup_border_style = "rounded", -- Rounded border for floating windows
+      enable_git_status = true, -- Show git status in the tree
+      enable_diagnostics = true, -- Show diagnostics in the tree
+      follow_current_file = true,
+		})
 	end,
 }
